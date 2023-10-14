952a6fe84d9ac205e4b1c4f9ffd40b50b3b74dd3
akash <akashamrolkar10@gmail.com>
2023-10-14 14:55:45 +0530

    Initial Changes

--- frontend/src/pages/doctors/DoctorDetails.jsx
+++ frontend/src/pages/doctors/DoctorDetails.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import doctorImg from '../../assets/images/doctor-img02.png'
 const DoctorDetails = () => {
   return (
-    <div>DoctorDetails</div>
+    <>
+      <section className='py-10'>
+        <div className='max-w-[1170px] px-5 mx-auto'>
+          <div className='grid md:grid-cols-3 gap-[50px]'>
+            <div className='md:col-span-2'>
+              <div className='flex items-center gap-5'>
+                <figure>
+                  <img src={doctorImg} alt='' className='w-full' />
+                </figure>
+                <div className='flex flex-col gap-4'>
+                  <span className='bg-[#ccf0f3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded'>Surgeon</span>
+                  <h3 className='text-headingColor text-[22px] font-bold'>Muhibur Rahman</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- frontend/src/pages/doctors/Doctors.jsx
+++ frontend/src/pages/doctors/Doctors.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import doctors from '../../assets/data/doctors'
+import DoctorComponent from '../../component/DoctorComponent'
 
 const Doctors = () => {
+  const [val, setVal] = useState('');
+  const handleChange = (e)=>{
+    e.preventDefault();
+    setVal(e.target.value)
+    
+  }
   return (
-    <div>Doctors</div>
+    <>
+    <section className=' bg-[#fff9ea] text-center py-20'>
+      <div className='container mx-auto'>
+        <h2 className='heading text-4xl'>Find a Doctor</h2>
+        <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
+          <input 
+            type='search'
+            className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor'
+            placeholder='Search Doctor'
+            onChange={handleChange} value={val} />
+            <button className='btn mt-o rounded-[0px] rounded-r-md bg-blue-600 py-4 px-3 text-white flex justify-center font-bold'>Search</button>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 py-16 items-center'>
+          {
+            doctors.map((Doctor, index)=>{
+                  return(
+                      <DoctorComponent data={Doctor} key={index} index={index} />
+                  )
+              })
+          }
+        </div>
+      </div>
+    </section>
+    </>
+    
+    
   )
 }
 
