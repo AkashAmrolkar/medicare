85a6fe6807b8c79bed685a88c3064272911fc642
akash <akashamrolkar10@gmail.com>
2023-10-21 15:28:36 +0530

    Backend Connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 const app = express();
 const PORT = 5000;
 
-app.listen( PORT ,()=>{
-    console.log(`server is running at port number ${PORT}`)
-});
+mongoose.connect('mongodb+srv://akashamrolkar10:Hq1gBsgg9dWT725m@cluster0.zd8t27o.mongodb.net/?retryWrites=true&w=majority').then(()=> app.listen(PORT)).then(()=> console.log('Connected with mongodb')).catch((e)=> console.log(e))
