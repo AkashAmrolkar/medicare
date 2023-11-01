79be13686dcf5f47e954410c1a7f98110eb8fe89
akash <akashamrolkar10@gmail.com>
2023-11-02 00:31:54 +0530

    registration final

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import React from 'react'
 
 const SignUp = () => {
   const [FormData, setFormData] = useState({
-    first_name: '',
-    last_name: '',
+    firstname: '',
+    lastname: '',
     email: '',
-    contact: '',
     role: '',
     gender: '',
+    profileimg: ''
   })
 
   const handleChange = (e) => {
@@ -18,10 +17,11 @@ const SignUp = () => {
       [e.target.name] : e.target.value
     })
   }
-
-  const handleSubmit = async(FormData) =>{
+  console.log(FormData)
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
     try {
-      const response = await fetch('/api/form/submit', {
+      const response = await fetch('http://localhost:8000/api/users/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -45,11 +45,11 @@ const SignUp = () => {
       <form action='' onSubmit={handleSubmit} className='flex flex-col justify-center items-center gap-4 bg-white shadow px-10 py-12 w-fit mx-auto'>
         <div className='filed mb-4 flex flex-col gap-2'>
           <label htmlFor='first_name'>First Name:</label>
-          <input className='h-10 border px-3 border-teal-500 rounded-3xl' id='first_name' type='text' value={FormData.first_name} onChange={handleChange} name='first_name' placeholder='First Name' required />
+          <input className='h-10 border px-3 border-teal-500 rounded-3xl' id='first_name' type='text' value={FormData.firstname} onChange={handleChange} name='firstname' placeholder='First Name' required />
         </div>
         <div className='filed mb-4 flex flex-col gap-2'>
           <label htmlFor='last_name'>Last Name:</label>
-          <input className='h-10 border px-3 border-teal-500 rounded-3xl' id='last_name' type='text' value={FormData.last_name} onChange={handleChange} name='last_name' placeholder='Last Name' required />
+          <input className='h-10 border px-3 border-teal-500 rounded-3xl' id='last_name' type='text' value={FormData.lastname} onChange={handleChange} name='lastname' placeholder='Last Name' required />
         </div>
         <div className='filed mb-4 flex flex-col gap-2'>
           <label htmlFor='email'>Email:</label>
@@ -71,6 +71,10 @@ const SignUp = () => {
             <option value="female">Female</option>
             <option value="other">Other</option>
           </select>
+        </div>
+        <div className='filed mb-4 flex flex-col gap-2'>
+          <label htmlFor='profileimg'>Profile Image:</label>
+          <input className='h-10 border px-3 border-teal-500 rounded-3xl' id='profileimg' type='text' value={FormData.profileimg} onChange={handleChange} name='profileimg' placeholder='Profile Image' required />
         </div>
         <button type='submit' className=' bg-teal-500 border border-transparent rounded-3xl text-xl font-medium text-white px-8 py-2'>Login</button>
       </form>
